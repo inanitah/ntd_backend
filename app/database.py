b2c6758ce69d54755568dc4338577634ef44f28b
Mike Radzewicz <mikerad1989@gmail.com>
2024-06-29 17:21:16 -0300

    Add full db url

--- app/database.py
+++ app/database.py
@@ -1,15 +1,22 @@
 import mysql.connector
 from mysql.connector import Error
 
+# FULL_DB_URL = "mysql://qwq44n23q3iz0jjj:miik1fpwkjr8qek9@tk3mehkfmmrhjg0b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/vqh46dlxyis6rini"
+
+HOST = 'tk3mehkfmmrhjg0b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
+DATABASE = 'vqh46dlxyis6rini'
+USER = 'qwq44n23q3iz0jjj'
+PASSWORD = 'miik1fpwkjr8qek9'
+
 
 def create_connection():
     connection = None
     try:
         connection = mysql.connector.connect(
-            host='localhost',
-            database='calculator',
-            user='calculator_user',
-            password='password'
+            host=HOST,
+            database=DATABASE,
+            user=USER,
+            password=PASSWORD
         )
     except Error as e:
         print(f"The error '{e}' occurred")
